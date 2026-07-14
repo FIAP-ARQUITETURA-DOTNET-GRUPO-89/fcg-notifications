b859e7b20fbdedd18beb4b54ed27d2f13a9416b3
Camila Melo Machado <camila_melomachado@hotmail.com>
2026-07-14 00:45:35 -0300

    ✨ feat: criar migração e configura migration automatica

---
+++ src/FcgNotifications.Worker/Extensions/MigrationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using FcgNotifications.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace FcgNotifications.Worker.Extensions;
+
+public static class MigrationExtensions
+{
+    public static async Task ApplyMigrationsAsync(this IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+
+        var environment = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
+
+        var runMigration = environment.IsDevelopment() || (environment.IsProduction() && Environment.GetEnvironmentVariable("RUN_MIGRATION") == "true");
+
+        if (!runMigration)
+        {
+            return;
+        }
+
+        var db = scope.ServiceProvider.GetRequiredService<FcgNotificationsDbContext>();
+
+        if (db.Database.IsRelational())
+        {
+            await db.Database.MigrateAsync();
+        }
+    }
+}
